3c1d0821a3fea53e8ada348c04bfe971834fcf79
Hemachandran SS <hemachandran@colanonline.com>
2026-08-22 21:21:32 +0530

    Woodland and Forest

--- assets/index.js
+++ assets/index.js
@@ -25,7 +25,8 @@ const visualBooks = [
     'books/visual-books/the_most_fantastic_atlas',
     'books/visual-books/dk_children_s_illustrated_history_atlas',
     'books/visual-books/science_squad',
-    'books/visual-books/weather_and_the_seasons'
+    'books/visual-books/weather_and_the_seasons',
+    'books/visual-books/woodfores'
 ];
 
 const bookCount = visualBooks.length;
