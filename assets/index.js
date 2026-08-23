a3da979281f5c114ec270e2d9dfbfb1ab7fa01e3
Hemachandran SS <hemachandran@colanonline.com>
2026-08-23 11:36:47 +0530

    Cooking Step By Step

--- assets/index.js
+++ assets/index.js
@@ -32,7 +32,8 @@ const visualBooks = [
     'books/visual-books/365_things_to_do_with_paper',
     'books/visual-books/domoney_d_my_house_plant_changed_my_life',
     'books/visual-books/the_crafter_s_year',
-    'books/visual-books/dk_children_lets_sew'
+    'books/visual-books/dk_children_lets_sew',
+    'books/visual-books/cooking_step_by_step'
 ];
 
 const bookCount = visualBooks.length;
