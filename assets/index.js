db262909e5adb3502359ae24b91884567d52c9ca
Hemachandran SS <hemachandran@colanonline.com>
2026-08-23 16:16:26 +0530

    Big Book Of Knowledge

--- assets/index.js
+++ assets/index.js
@@ -66,7 +66,8 @@ const visualBooks = [
     'books/visual-books/dk_smithsonian_ocean',
     'books/visual-books/Ann_Swanson_Science_of_Yoga_Understand_the_Anat',
     'books/visual-books/DK_-_1000_Words_-_Science',
-    'books/visual-books/Where_on_Earth_-_Dorling_Kindersley'
+    'books/visual-books/Where_on_Earth_-_Dorling_Kindersley',
+    'books/DK_-_Big_Book_of_Knowledge'
 ];
 
 const bookCount = visualBooks.length;
