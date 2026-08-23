d8c733714bfb3e3dc2c14aa0d34ca880aa096558
Hemachandran SS <hemachandran@colanonline.com>
2026-08-23 21:54:42 +0530

    First Space Encyclopedia

--- assets/index.js
+++ assets/index.js
@@ -74,7 +74,8 @@ const visualBooks = [
     'books/visual-books/Scientists_Who_Changed_History',
     'books/visual-books/Geography_of_the_World_By_DK',
     'books/visual-books/DK_-_Wonders_of_Nature_Ben_Hoare',
-    'books/visual-books/Super_Science_Encyclopedia_How_Science_Shapes_Our_World_by_Dorling_Kindersley'
+    'books/visual-books/Super_Science_Encyclopedia_How_Science_Shapes_Our_World_by_Dorling_Kindersley',
+    'books/visual-books/dk_first_space_encyclopedia'
 ];
 
 const bookCount = visualBooks.length;
